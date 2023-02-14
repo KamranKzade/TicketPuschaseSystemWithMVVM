2e0e4ef137f3cbdaff3d5502c73a4e3752f55ccb
Kamran <311.kamran@gmail.com>
2023-02-14 15:42:08 +0400

    Add project files.

---
+++ TicketPuschaseSystemWithMVVM/Domain/ViewModels/BaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace TicketPuschaseSystemWithMVVM.Domain.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
