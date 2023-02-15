0d0c7c53ead30c5d675a94881bef477b2371616b
Kamran <311.kamran@gmail.com>
2023-02-15 10:27:35 +0400

    Complete Front

---
+++ TicketPuschaseSystemWithMVVM/Domain/ViewModels/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TicketPuschaseSystemWithMVVM.Commads;
+
+namespace TicketPuschaseSystemWithMVVM.Domain.ViewModels
+{
+    public class MainViewModel:BaseViewModel
+    {
+        public RelayCommand Minimize_Btn { get; set; }
+        public RelayCommand Close_Btn { get; set; }
+
+
+        public MainViewModel()
+        {
+            Minimize_Btn = new RelayCommand((o) =>
+            {
+                var window = o as Window;
+                window.WindowState = WindowState.Minimized;
+            });
+
+            Close_Btn = new RelayCommand((o) =>
+            {
+                Application.Current.Shutdown();
+            });
+
+        }
+    }
+}
